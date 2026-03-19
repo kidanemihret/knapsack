0d2f5cac33e9f710a47b37c0fd5e584b9f918339
assefamaru <github@alexandermaru.com>
2026-03-18 21:58:53 -0400

    Randomize names and pick multi-draw winner

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"slices"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/mattschofield/go-knapsack"
 )
@@ -10,7 +18,7 @@ import (
 var capacity int64 = 1000
 
 var items = []item{
-	// Add each item (candidate) here.
+	// TODO
 }
 
 type item struct {
@@ -32,18 +40,69 @@ func (i item) Weight() int64 {
 }
 
 func main() {
-	var packables []knapsack.Packable
+	seen := make(map[string]struct{})
 	for _, item := range items {
+		if _, alreadySeen := seen[item.Name()]; alreadySeen {
+			// If there are duplicate names (which is possible)
+			// manually append some differentiable suffix to each duplicate
+			// name, to aid identification on selection.
+			log.Fatalf("Name %q already exists: please append some unique suffix to each duplicate name\n", item.Name())
+		}
+		seen[item.Name()] = struct{}{}
+	}
+
+	selected := make(map[string]int)
+	for {
+		picks := permuteAndSelect(items)
+		fingerprint := fingerprintFrom(picks)
+		selected[fingerprint] += 1
+		if selected[fingerprint] == 3 {
+			fmt.Printf("[%d] %s - winner!\n", selected[fingerprint], fingerprint)
+			// Stop when the first fingerprint to be selected 3 times is found.
+			printSummary(picks)
+			break
+		} else {
+			fmt.Printf("[%d] %s - try again\n", selected[fingerprint], fingerprint)
+		}
+	}
+}
+
+func permuteAndSelect(items []item) []item {
+	perm := make([]item, len(items))
+	for i, v := range rand.Perm(len(items)) {
+		perm[v] = items[i]
+	}
+
+	var packables []knapsack.Packable
+	for _, item := range perm {
 		packables = append(packables, item)
 	}
 
 	picks := knapsack.Knapsack(packables, capacity)
-	printSummary(items, picks, capacity)
+
+	var pickedItems []item
+	for _, i := range picks {
+		pickedItems = append(pickedItems, perm[i])
+	}
+	return pickedItems
 }
 
-func printSummary(items []item, picks []int64, capacity int64) {
-	fmt.Printf("Selected the following names (capacity: %d):", capacity)
-	for i, pick := range picks {
-		fmt.Printf("%d. Name: %s\n   Weight: %d\n", i+1, items[pick].Name(), items[pick].Weight())
+func fingerprintFrom(picks []item) string {
+	var selection []string
+	for _, pick := range picks {
+		selection = append(selection, pick.Name())
 	}
+	slices.Sort(selection)
+	hasher := md5.New()
+	hasher.Write([]byte(strings.Join(selection, "")))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func printSummary(picks []item) {
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+	fmt.Printf("\nSelected the following names (capacity: %d):\n", capacity)
+	for i, pick := range picks {
+		fmt.Fprintf(w, "%d. \tName: %s\tWeight: %d\n", i+1, pick.Name(), pick.Weight())
+	}
+	w.Flush()
 }
